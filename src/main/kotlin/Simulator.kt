b9f8dc9bfb8e2944673a52764bd5f5ef811ef9af
stormslowly@gmail.com <stormslowly@gmail.com>
2017-07-28 10:37:20 +0800

    👽  switch to opencv2

--- src/main/kotlin/Simulator.kt
+++ src/main/kotlin/Simulator.kt
@@ -6,17 +6,15 @@
 package com.ulong51
 
 import org.opencv.core.*
-import org.opencv.features2d.BFMatcher
-import org.opencv.imgcodecs.Imgcodecs
-import org.opencv.xfeatures2d.SURF
+
 import org.opencv.core.MatOfKeyPoint
 import org.opencv.features2d.FeatureDetector
-import org.opencv.core.KeyPoint
 import org.opencv.features2d.DescriptorExtractor
 import org.opencv.features2d.Features2d
 import org.opencv.core.Scalar
-import org.opencv.imgcodecs.Imgcodecs.CV_LOAD_IMAGE_COLOR
 import org.opencv.core.Mat
+import org.opencv.highgui.Highgui
+import org.opencv.highgui.Highgui.CV_LOAD_IMAGE_COLOR
 
 
 class Simulator {
@@ -37,47 +35,33 @@ fun main(args: Array<String>) {
     println(Core.VERSION)
 
 
-    val mat = Mat(10, 10, CvType.CV_8UC1, Scalar(0.0))
-
-    println(mat.row(1).col(1).setTo(Scalar(1.2)))
-    val sim1 = Simulator()
-
-    val put = mat.put(5, 5, 77.0)
-
-
-//    val featureDetector = FeatureDetector.create(FeatureDetector.SURF)
-    val featureDetector = SURF.create()
-
+    val featureDetector = FeatureDetector.create(FeatureDetector.SURF)
     val descExtractor = DescriptorExtractor.create(DescriptorExtractor.SURF)
 
-    DescriptorExtractor
-
-
-    val loginImage = Imgcodecs.imread("images/login.png")
-
-    val screenImage = Imgcodecs.imread("images/gamescreen.png")
+    var targetImage = Highgui.imread("images/login.png", CV_LOAD_IMAGE_COLOR)
+    val screenImage = Highgui.imread("images/gamescreen.png",CV_LOAD_IMAGE_COLOR)
 
     val targetFeature = MatOfKeyPoint()
     val backgroundFeature = MatOfKeyPoint()
 
-    featureDetector.detect(loginImage, targetFeature)
+    featureDetector.detect(targetImage, targetFeature)
 
 
     val targetDescripto = MatOfKeyPoint()
-    descExtractor.compute(loginImage, targetFeature, targetDescripto)
+    descExtractor.compute(targetImage, targetFeature, targetDescripto)
 
-    val outputImage = Mat(loginImage.rows(), loginImage.cols(), CV_LOAD_IMAGE_COLOR)
+    val outputImage = Mat(targetImage.rows(), targetImage.cols(), CV_LOAD_IMAGE_COLOR)
     val newKeypointColor = Scalar(255.0, 0.0, 0.0)
 
-    val keyPoints = targetFeature.toArray()
-
-
     println("Drawing key points on object image...")
-    Features2d.drawKeypoints(loginImage, targetFeature, outputImage, newKeypointColor, 0)
+    Features2d.drawKeypoints(targetImage, targetFeature, outputImage, newKeypointColor, 0)
+
+    Highgui.imwrite("testout.png", outputImage)
 
 
-    Imgcodecs.imwrite("testout.png", outputImage)
 
+    val sceenKeyPoint =  MatOfKeyPoint()
+    val sceenKeyDescripto =  MatOfKeyPoint()
 
 //
 //    featureDetector.detect(screenImage, backgroundFeature)
